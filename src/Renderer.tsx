2bc656b33dcd49431caf581fa0b365f1b4a0e8f5
Logan <logan.zartman@utexas.edu>
2024-09-28 13:18:27 -0700

    uniforms

--- src/Renderer.tsx
+++ src/Renderer.tsx
@@ -62,23 +62,70 @@ export function Renderer({
     [context]
   );
 
+  const ubSize = 4 * (4 + 2 + 2);
+  const ubColorOffset = 0;
+  const ubScaleOffset = 4;
+  const ubOffsetOffset = 6;
+
+  const uniformValues = useMemo(() => {
+    const uv = new Float32Array(ubSize / 4);
+    uv.set([1, 1, 0, 1], ubColorOffset);
+    uv.set([1, 1], ubScaleOffset);
+    uv.set([-0.5, -0.25], ubOffsetOffset);
+    return uv;
+  }, [ubOffsetOffset, ubScaleOffset, ubSize]);
+
+  const uniformBuffer = useMemo(() => {
+    const ub = device.createBuffer({
+      size: ubSize,
+      usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+    });
+    return ub;
+  }, [device, ubSize]);
+
+  const bindGroup = useMemo(
+    () =>
+      device.createBindGroup({
+        layout: pipeline.getBindGroupLayout(0),
+        entries: [{ binding: 0, resource: { buffer: uniformBuffer } }],
+      }),
+    [device, pipeline, uniformBuffer]
+  );
+
   useAnimationFrame(
     useCallback(() => {
+      const t = Date.now() / 1000;
       renderPassDescriptor.colorAttachments[0].view = context
         .getCurrentTexture()
         .createView();
+
+      const aspectRatio = context.canvas.width / context.canvas.height;
+      uniformValues.set([0.5 / aspectRatio, 0.5], ubScaleOffset);
+      uniformValues.set([Math.sin(t) * 0.5, Math.cos(t) * 0.5], ubOffsetOffset);
+
+      device.queue.writeBuffer(uniformBuffer, 0, uniformValues);
 
       const encoder = device.createCommandEncoder({
         label: "basic command encoder",
       });
       const pass = encoder.beginRenderPass(renderPassDescriptor);
       pass.setPipeline(pipeline);
+      pass.setBindGroup(0, bindGroup);
       pass.draw(3);
       pass.end();
 
       const commandBuffer = encoder.finish();
       device.queue.submit([commandBuffer]);
-    }, [context, device, pipeline, renderPassDescriptor])
+    }, [
+      bindGroup,
+      context,
+      device,
+      pipeline,
+      renderPassDescriptor,
+      ubScaleOffset,
+      uniformBuffer,
+      uniformValues,
+    ])
   );
 
   return null;
--- src/redTri.wgsl
+++ src/redTri.wgsl
@@ -1,3 +1,11 @@
+struct Data {
+  color: vec4f,
+  scale: vec2f,
+  offset: vec2f,
+};
+
+@group(0) @binding(0) var<uniform> data: Data;
+
 @vertex fn vs(
   @builtin(vertex_index) vertexIndex : u32
 ) -> @builtin(position) vec4f {
@@ -7,9 +15,9 @@
     vec2f( 0.5, -0.5)   // bottom right
   );
   
-  return vec4f(pos[vertexIndex], 0.0, 1.0);
+  return vec4f(pos[vertexIndex] * data.scale + data.offset, 0.0, 1.0);
 }
 
 @fragment fn fs() -> @location(0) vec4f {
-  return vec4f(1.0, 0.0, 0.0, 1.0);
+  return data.color;
 }
